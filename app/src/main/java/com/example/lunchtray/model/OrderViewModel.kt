252b9d7f2a2fa5cbc0e766dd364434eb1cf8764b
Pimentel <EPIMENT5@ford.com>
2021-11-21 18:31:23 -0300

    funcao resetOrder corrigida

--- app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -171,8 +171,8 @@ class OrderViewModel : ViewModel() {
         _entree.value = null
         _side.value = null
         _accompaniment.value = null
-        _subtotal.value = null
-        _total.value = null
-        _tax.value = null
+        _subtotal.value = 0.0
+        _total.value = 0.0
+        _tax.value = 0.0
     }
 }
